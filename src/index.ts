f3ca901af8902da6a77e28965c57d2c24199ef17
Thomas Lekanger <datagutt@lekanger.no>
2020-02-08 03:24:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import { Client } from "./Client";
 import {
-    CommandService,
-    ModeService,
-    StateService
+	CommandService,
+	ModeService,
+	StateService
 } from "./services";
 
 /**
  * Pynab API class
  */
 export default class Pynab {
-    public client: Client;
-    public Command: CommandService;
-    public Mode: ModeService;
-    public State: StateService;
+	public client: Client;
+	public Command: CommandService;
+	public Mode: ModeService;
+	public State: StateService;
 
-    constructor (baseUrl: string) {
-        this.client = new Client(baseUrl);
-        this.Command = new CommandService(this.client);
-        this.Mode = new ModeService(this.client);
-        this.State = new StateService(this.client);
-    }
+	constructor(baseUrl: string) {
+		this.client = new Client(baseUrl);
+		this.Command = new CommandService(this.client);
+		this.Mode = new ModeService(this.client);
+		this.State = new StateService(this.client);
+	}
 }
